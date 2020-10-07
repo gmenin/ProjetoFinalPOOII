195bab2caaa8e6508f45118bf0c066c5afee23d4
dedeg <dedeg@192.168.56.1>
2020-10-07 14:43:21 -0300

    Classe Formulário e Classe Usuário criadas Com seus devidos construtores, gets e sets

---
+++ ProjetoFinalPOOII/src/modelo/Formulario.java
@@ -0,0 +1,152 @@
+package modelo;
+
+import java.util.Date;
+
+public class Formulario {
+	
+	private String solicitante;
+	private String telefone;
+	private String email;
+	private int numeroAlunos;
+	private String atividade;
+	private String modalidade;
+	private String curso;
+	private String equipamentos;
+	private Date [] data = new Date[6];
+	private int [] horaInicial = new int[6];
+	private int [] horaFinal = new int[6];
+	
+	
+	public Formulario(String solicitante, String telefone, String email, int numeroAlunos, String atividade,
+			String modalidade, String curso, String equipamentos, Date[] data, int[] horaInicial, int[] horaFinal) {
+		super();
+		this.solicitante = solicitante;
+		this.telefone = telefone;
+		this.email = email;
+		this.numeroAlunos = numeroAlunos;
+		this.atividade = atividade;
+		this.modalidade = modalidade;
+		this.curso = curso;
+		this.equipamentos = equipamentos;
+		this.data = data;
+		this.horaInicial = horaInicial;
+		this.horaFinal = horaFinal;
+	}
+	
+	@SuppressWarnings("null")
+	public Formulario() {
+		super();
+		this.solicitante = null;
+		this.telefone = null;
+		this.email = null;
+		this.numeroAlunos = (Integer) null;
+		this.atividade = null;
+		this.modalidade = null;
+		this.curso = null;
+		this.equipamentos = null;
+		this.data = null;
+		this.horaInicial = null;
+		this.horaFinal = null;
+	}
+	
+	//-------------------------------------------------
+	public String getSolicitante() {
+		return solicitante;
+	}
+	public void setSolicitante(String solicitante) {
+		this.solicitante = solicitante;
+	}
+	//-------------------------------------------------
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	//-------------------------------------------------
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	//-------------------------------------------------
+	public int getNumeroAlunos() {
+		return numeroAlunos;
+	}
+	public void setNumeroAlunos(int numeroAlunos) {
+		this.numeroAlunos = numeroAlunos;
+	}
+	//-------------------------------------------------
+	public String getAtividade() {
+		return atividade;
+	}
+	public void setAtividade(String atividade) {
+		this.atividade = atividade;
+	}
+	//-------------------------------------------------
+	public String getModalidade() {
+		return modalidade;
+	}
+	public void setModalidade(String modalidade) {
+		this.modalidade = modalidade;
+	}
+	//-------------------------------------------------
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	//-------------------------------------------------
+	public String getEquipamentos() {
+		return equipamentos;
+	}
+	public void setEquipamentos(String equipamentos) {
+		this.equipamentos = equipamentos;
+	}
+	//-------------------------------------------------
+	public Date[] getData() {
+		return data;
+	}
+	public Date getData(int index) {
+		return data[index];
+	}
+		
+	public void setData(Date[] data) {
+		this.data = data;
+	}
+	public void setData(int index, Date data) {
+		this.data[index] = data;
+	}
+	//-------------------------------------------------
+	public int[] getHoraInicial() {
+		return horaInicial;
+	}
+	public int getHoraInicial(int index) {
+		return horaInicial[index];
+	}
+		
+	public void setHoraInicial(int[] horaInicial) {
+		this.horaInicial = horaInicial;
+	}
+	public void setHoraInicial(int index, int horaInicial) {
+		this.horaInicial[index] = horaInicial;
+	}
+	//-------------------------------------------------	
+	public int[] getHoraFinal() {
+		return horaFinal;
+	}
+	public int getHoraFinal(int index) {
+		return horaFinal[index];
+	}
+	
+	public void setHoraFinal(int[] horaFinal) {
+		this.horaFinal = horaFinal;
+	}
+	public void setHoraFinal(int index, int horaFinal) {
+		this.horaFinal[index] = horaFinal;
+	}
+	//-------------------------------------------------
+
+}
---
+++ ProjetoFinalPOOII/src/modelo/Usuario.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Usuario {
+
+	private String usuario;
+	private String senha;
+	
+	public Usuario(String usuario, String senha) {
+		super();
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	public Usuario() {
+		super();
+		this.usuario = null;
+		this.senha = null;
+	}
+
+	//-------------------------------------------------
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	//-------------------------------------------------
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	//-------------------------------------------------
+	
+}
