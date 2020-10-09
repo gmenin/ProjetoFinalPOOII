68384313cf95090e28007511d2af8b0b20799d8e
meuplow <gabr.menin@gmail.com>
2020-10-09 02:39:36 -0300

    Inicio da implementacao de métodos na classe FormularioDAO

--- ProjetoFinalPOOII/src/dao/FormularioDAO.java
+++ ProjetoFinalPOOII/src/dao/FormularioDAO.java
@@ -1,6 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modelo.Formulario;
 
 public class FormularioDAO {
 
@@ -9,6 +13,41 @@ public class FormularioDAO {
 	public FormularioDAO() {
 		
 	}
-
 	
+	//Me'todo que realiza a insercao do formulario no banco
+		public boolean inserirFormulario(Formulario formulario) {
+			ConexaoMySQL.abrirConexao();
+			con = ConexaoMySQL.getCon();
+
+			if(con != null) {
+				String sql = "INSERT INTO VALUES(?,?,?,?,?,?)";
+
+				PreparedStatement prepareStatement = null; //had to fix
+				try {
+					prepareStatement = con.prepareStatement(sql);
+					prepareStatement.setString(1, formulario.getSolicitante());
+					prepareStatement.setString(2, formulario.getTelefone());
+					prepareStatement.setString(3, formulario.getEmail());
+					prepareStatement.setInt(4, formulario.getNumeroAlunos());
+					prepareStatement.setString(5, formulario.getAtividade());
+					prepareStatement.setString(5, formulario.getCurso());
+					prepareStatement.setString(5, formulario.getEquipamentos());
+
+					int resultado = prepareStatement.executeUpdate();
+
+					if(resultado == 1) {
+						ConexaoMySQL.fecharConexao();
+						return true;
+					}else {
+						ConexaoMySQL.fecharConexao();
+						return false;
+					}
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+			return false;
+		}
 }
