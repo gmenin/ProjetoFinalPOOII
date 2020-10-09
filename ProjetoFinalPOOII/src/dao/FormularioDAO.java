c4047f53939ebf942cba248ba29788259cd6d82a
meuplow <gabr.menin@gmail.com>
2020-10-09 12:21:19 -0300

    Atualizacao dos comentarios da classe FormularioDAO

--- ProjetoFinalPOOII/src/dao/FormularioDAO.java
+++ ProjetoFinalPOOII/src/dao/FormularioDAO.java
@@ -12,45 +12,49 @@ public class FormularioDAO {
 	private Connection con = null;
 
 	public FormularioDAO() {
-		
+
 	}
-	
-	//Me'todo que realiza a insercao dos dados do formulario no banco
-		public boolean inserirFormulario(Formulario formulario, int usuarioId) {
-			ConexaoMySQL.abrirConexao();
-			con = ConexaoMySQL.getCon();
 
-			if(con != null) {
-				String sql = "INSERT INTO requisicao (usuarioId, solicitante, telefone, email, numeroAlunos, atividade, modalidade, curso, equipamentos) VALUES(?,?,?,?,?,?,?,?,?)";
+	// Me'todo que realiza a insercao dos dados do formulario no banco
+	public boolean inserirFormulario(Formulario formulario, int usuarioId) {
+		ConexaoMySQL.abrirConexao();
+		con = ConexaoMySQL.getCon();
 
-				PreparedStatement prepareStatement = null; 
-				try {
-					prepareStatement = con.prepareStatement(sql);
-					prepareStatement.setInt(1, usuarioId);
-					prepareStatement.setString(2, formulario.getSolicitante());
-					prepareStatement.setString(3, formulario.getTelefone());
-					prepareStatement.setString(4, formulario.getEmail());
-					prepareStatement.setInt(5, formulario.getNumeroAlunos());
-					prepareStatement.setString(6, formulario.getAtividade());
-					prepareStatement.setString(7, formulario.getModalidade());
-					prepareStatement.setString(8, formulario.getCurso());
-					prepareStatement.setString(9, formulario.getEquipamentos());
+		if(con != null) {
+			String sql = "INSERT INTO requisicao (usuarioId, solicitante, telefone, email, numeroAlunos, atividade, modalidade, curso, equipamentos) VALUES(?,?,?,?,?,?,?,?,?)";	
+			PreparedStatement prepareStatement = null; 
+			
+			try {
+				// Argumentos que serao inseridos no banco de dados
+				prepareStatement = con.prepareStatement(sql);
+				prepareStatement.setInt(1, usuarioId);
+				prepareStatement.setString(2, formulario.getSolicitante());
+				prepareStatement.setString(3, formulario.getTelefone());
+				prepareStatement.setString(4, formulario.getEmail());
+				prepareStatement.setInt(5, formulario.getNumeroAlunos());
+				prepareStatement.setString(6, formulario.getAtividade());
+				prepareStatement.setString(7, formulario.getModalidade());
+				prepareStatement.setString(8, formulario.getCurso());
+				prepareStatement.setString(9, formulario.getEquipamentos());
 
-					int resultado = prepareStatement.executeUpdate();
-
-					if(resultado == 1) {
-						ConexaoMySQL.fecharConexao();
-						return true;
-					}else {
-						ConexaoMySQL.fecharConexao();
-						return false;
-					}
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+				int resultado = prepareStatement.executeUpdate();
+				
+				// Retorna true se a insercao ocorreu com sucesso e false caso tenha falhado
+				if(resultado == 1) {
+					ConexaoMySQL.fecharConexao();
+					return true;
+				}else {
+					ConexaoMySQL.fecharConexao();
+					return false;
 				}
-
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-			return false;
-		}
+
+		} // if
+		return false;
+	} // inserirFormulario()
+	
+	
 }
