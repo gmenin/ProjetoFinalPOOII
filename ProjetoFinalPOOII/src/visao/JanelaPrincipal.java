746d9d35fdc76734263605322bb618dd017562e0
maicotc <triacamaico@gmail.com>
2020-10-09 20:44:42 -0300

    Começando implementação da JanelaPrincipal

--- ProjetoFinalPOOII/src/visao/JanelaPrincipal.java
+++ ProjetoFinalPOOII/src/visao/JanelaPrincipal.java
@@ -6,6 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JButton;
+import java.awt.Color;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class JanelaPrincipal extends JFrame {
 
@@ -31,12 +38,23 @@ public class JanelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public JanelaPrincipal() {
+		setTitle("Formulario Digital de Reservas de Sala de Aula");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 850, 650);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel painelLogin = new JPanel();
+		painelLogin.setBackground(new Color(51, 102, 153));
+		contentPane.add(painelLogin, "name_95302867270400");
+		painelLogin.setLayout(new MigLayout("", "[572px]", "[97px]"));
+		
+		JLabel labelTituloPainelLogin = new JLabel("Formul\u00E1rio Digital para Reserva de Sala de Aula");
+		labelTituloPainelLogin.setForeground(new Color(255, 255, 255));
+		labelTituloPainelLogin.setFont(new Font("Arial", Font.BOLD, 26));
+		painelLogin.add(labelTituloPainelLogin, "cell 0 0,grow");
 	}
 
 }
