29cd60bd7836339761adf8ee56ef87c8da0d314f
maicotc <triacamaico@gmail.com>
2020-10-09 20:08:18 -0300

    Nova JanelaPrincipal

--- ProjetoFinalPOOII/src/visao/JanelaPrincipal.java
+++ ProjetoFinalPOOII/src/visao/JanelaPrincipal.java
@@ -6,30 +6,37 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
-import java.awt.CardLayout;
 
 public class JanelaPrincipal extends JFrame {
 
 	private JPanel contentPane;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaPrincipal frame = new JanelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
 	public JanelaPrincipal() {
-		setTitle("Formulario Digital para Reservas de Aula");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 850, 650);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(51, 102, 153));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		contentPane.setLayout(new CardLayout(0, 0));
-		contentPane.setVisible(false);
-		
-		
-		JPanel paninelInicial = new JPanel();
-		paninelInicial.setVisible(true);
-		contentPane.add(paninelInicial, "name_91394305391200");
-		
-		
 	}
 
 }
