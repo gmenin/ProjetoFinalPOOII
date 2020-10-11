15dc833e5dc3a8a95189ed21507328b4a2b35c08
dedeg <dedeg@192.168.56.1>
2020-10-11 16:06:03 -0300

    tornar não redimensionavel

--- ProjetoFinalPOOII/src/visao/JanelaPrincipal.java
+++ ProjetoFinalPOOII/src/visao/JanelaPrincipal.java
@@ -73,6 +73,7 @@ public class JanelaPrincipal extends JFrame {
 
 	@SuppressWarnings("unchecked")
 	public JanelaPrincipal() {
+		setResizable(false);
 		setTitle("Formulario Digital de Reservas de Sala de Aula");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 850, 650);
