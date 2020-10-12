686c225c2f23e482efd867fa876cc4bad93e30a0
meuplow <gabr.menin@gmail.com>
2020-10-11 21:00:52 -0300

    Implementacao FormularioControle

--- ProjetoFinalPOOII/src/controle/FormularioControle.java
+++ ProjetoFinalPOOII/src/controle/FormularioControle.java
@@ -1,17 +1,32 @@
 package controle;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import dao.FormularioDAO;
 import visao.JanelaPrincipal;
 
-public class FormularioControle {
+public class FormularioControle implements ActionListener{
 
 	private JanelaPrincipal janelaPrincipal;
 	private FormularioDAO formularioDAO;
 
-	public FormularioControle(JanelaPrincipal j) {
-
-		this.janelaPrincipal = j;
+	public FormularioControle(JanelaPrincipal janelaPrincipal) {
+		this.janelaPrincipal = janelaPrincipal;
 		this.formularioDAO = new FormularioDAO();
+		this.janelaPrincipal.getButtonFazerReserva().addActionListener(this);
+	}
+
+	public void iniciarFormulario() {
+		this.janelaPrincipal.trocarPainel("painelInicioFormulario");
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		// TODO Auto-generated method stub
+		if(evento.getActionCommand().equals("Reservar")) {
+			iniciarFormulario();
+		}
 	}
 
 }
